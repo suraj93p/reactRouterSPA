c03558299521fb605a15533cc24584cb95a2311e
Suraj Pasayat <suraj.pasayat@deskera.com>
2020-06-14 02:12:33 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Switch, Route, Link } from "react-router-dom";
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import './App.css';
 
-function App() {
+const Home = () => <div>You're on the Home Tab</div>;
+const Comments = () => <div>You're on the Comments Tab</div>;
+const Contact = () => <div>You're on the Contact Tab</div>;
+
+const App = ({ match, history }) => {
+  const { path } = match;
+  const [value, setValue] = useState(0);
+  useEffect(() => {
+    let value = 0;
+    if (history.location.pathname === `${path}` || history.location.pathname === `${path}/home`) {
+      value = 0;
+    }
+    else if (history.location.pathname === `${path}/comments`) {
+      value = 1;
+    } else if (history.location.pathname === `${path}/contact`) {
+      value = 2;
+    }
+    setValue(value);
+  }, [history]);
+  const handleChange = (event, newValue) => {
+    newValue === 0 && history.push(`${path}`);
+    newValue === 1 && history.push(`${path}/comments`);
+    newValue === 2 && history.push(`${path}/contact`);
+    setValue(newValue);
+  };
+  const handleLogoClick = () => {
+    history.push(`${path}`);
+    setValue(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hey welcome home!</h1>
+      <div onClick={() => handleLogoClick()} className="logo">
+        LOGO
+      </div>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+      <div className="links">
+        <Link to={`${path}`} className="link">Home</Link>
+        <Link to={`${path}/comments`} className="link">Comments</Link>
+        <Link to={`${path}/contact`} className="link">Contact</Link>
+      </div>
+      <div className="tabs">
+        <Switch>
+          <Route path={`${path}`} exact component={Home} />
+          <Route path={`${path}/comments`} component={Comments} />
+          <Route path={`${path}/contact`} component={Contact} />
+        </Switch>
+      </div>
     </div>
   );
 }
